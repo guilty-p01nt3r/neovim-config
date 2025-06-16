d8011e3021006c6655571de714189783770b0e47
Vincenzo Simeone <vs994.programming@gmail.com>
2025-06-16 23:34:52 +0200

    chore: fixed the configuration for vtls

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,5 +1,5 @@
-local utils = require('guilty-p01nt3r.utils')
-local vue_language_server_path = utils.get_mason_executable('vue-language-server')
+local vue_language_server_path = vim.fn.stdpath('data') ..
+'/mason/packages/vue-language-server/node_modules/@vue/language-server'
 
 local vue_plugin = {
   name = '@vue/typescript-plugin',
